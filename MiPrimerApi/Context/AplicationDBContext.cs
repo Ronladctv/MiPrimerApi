b52e361fb3f9ea89230cfe4d553a9b5903b9b5ba
Ronladctv <ronald.carguaquispe@gmail.com>
2024-06-12 23:31:45 -0500

    Agregar archivos de proyecto.

---
+++ MiPrimerApi/Context/AplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiPrimerApi.Models;
+
+namespace MiPrimerApi.Context
+{
+    public class AplicationDBContext : DbContext
+    {
+        public DbSet<Producto> Producto { get; set; }
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options)
+        : base(options)
+        {
+        }
+    }
+}
---
+++ MiPrimerApi/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace MiPrimerApi.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price{ get; set; }
+        public int Stock { get; set; }
+
+    }
+}
